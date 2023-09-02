a43403a49b634334b835f66a3cd0db8ecdbb90a1
Jonathan Booker <1131478+JonnyBooker@users.noreply.github.com>
2023-09-02 18:03:48 +0100

    Initial commit of code

---
+++ src/NSubstitute.AutoSub/Extensions/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSubstitute.AutoSub.Extensions;
+
+internal static class TypeExtensions
+{
+    internal static bool IsCollection(this Type type)
+    {
+        if (type == typeof(string))
+        {
+            return false;
+        }
+
+        var containsInterfaceCollection = type.GetInterfaces().Any(i => i == typeof(IEnumerable));
+        return containsInterfaceCollection;
+    }
+    
+    internal static IList CreateListForType(this Type type)
+    {
+        var existingListType = typeof(List<>).MakeGenericType(type);
+        return (IList)Activator.CreateInstance(type);
+    }
+}
